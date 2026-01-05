6eb4a085f2e11cfb4be34fcd8ed34371ec062676
Ridho Rifqy Muhammad <ridhorifqy34@gmail.com>
2026-01-05 18:31:34 +0700

    Change the route from null to product

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/v1", productRouter);
+app.use("/api/v1/product", productRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening to PORT ${PORT}`);
